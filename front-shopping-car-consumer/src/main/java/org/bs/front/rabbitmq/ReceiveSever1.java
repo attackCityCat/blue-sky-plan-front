4770b9070f5c9d55773f69630558552280a7aa2c
attackCityCat <llpbzg@163.com>
2019-05-22 11:36:32 +0800

    2019年5月22日 11:35:12 修改购物车 下架时删除购物车下架的商品 修复后台价格改变时 购物车价格不变得问题 llp

--- front-shopping-car-consumer/src/main/java/org/bs/front/rabbitmq/ReceiveSever1.java
+++ front-shopping-car-consumer/src/main/java/org/bs/front/rabbitmq/ReceiveSever1.java
@@ -12,29 +12,29 @@ import java.util.Iterator;
 import java.util.Set;
 
 @Component
-@RabbitListener(queues = "delShop")//监听队列 属性值为队列的名字
+@RabbitListener(queues = "delShopa")//监听队列 属性值为队列的名字*/
 public class ReceiveSever1 {
 
     @Resource
     RedisTemplate<String, Object> redisTemplate;
 
-    /**
-     * 将下架的商品从购物车删除
-     * @param msg
-     */
+
     @RabbitHandler
-    public void getMsg(String msg) {
+    public void getMsg1(String msg) {
         System.out.println("我是消费者，我接收到了传递过来的数据为“" + msg + "”");
 
         //获取所有key
-        Set<String> keys = redisTemplate.keys(ConstantClass.FIND_USER_SHOP_CAR + "*");
+       Set<String> keys = redisTemplate.keys(ConstantClass.FIND_USER_SHOP_CAR + "*");
         System.out.println(keys);
         //迭代器迭代获取到的key
-        Iterator<String> iterator = keys.iterator();
+       Iterator<String> iterator = keys.iterator();
         while (iterator.hasNext()) {
             String key = iterator.next();
             //删除 所有用户中对应的商品
-            redisTemplate.opsForHash().delete(key, ConstantClass.SHOP_KEY + msg);
+            boolean hashKeyBoolean = redisTemplate.opsForHash().hasKey(key,ConstantClass.SHOP_KEY + msg);
+            if(hashKeyBoolean){
+                redisTemplate.opsForHash().delete(key, ConstantClass.SHOP_KEY + msg);
+            }
         }
     }
 }
